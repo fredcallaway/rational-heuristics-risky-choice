0ded5ea01093d49837572e4682fb5162bc19c44c
fredcallaway <fredc@llaway.com>
2026-06-29 09:04:01 -0400

    fix cost-only exclusion for logistic stats

--- R/logistic_regression.R
+++ R/logistic_regression.R
@@ -4,63 +4,37 @@ dat <- read.csv(args[2])   # path to processed human trials.csv
 
 library(lme4)
 
-dat <- dat[dat$cost > 0,]
-
 rank_desc <- function(x) {
     match(x, sort(unique(x), decreasing = TRUE)) - 1
 }
 
-dat$sigma <- match(dat$sigma, sort(unique(dat$sigma))) - 1
-dat$alpha <- rank_desc(dat$alpha)
-dat$cost <- rank_desc(dat$cost)
-for (strategy in c("TTB_SAT", "SAT_TTB", "TTB", "WADD", "Rand", "Other")) {
-    dat[[strategy]] <- as.integer(dat[[strategy]] == "True")
+prepare_data <- function(df) {
+    df$sigma <- match(df$sigma, sort(unique(df$sigma))) - 1
+    df$alpha <- rank_desc(df$alpha)
+    df$cost <- rank_desc(df$cost)
+    for (strategy in c("TTB_SAT", "SAT_TTB", "TTB", "WADD", "Rand", "Other")) {
+        df[[strategy]] <- as.integer(df[[strategy]] == "True")
+    }
+    df
 }
 
-s1 <- glmer(TTB_SAT ~ sigma + alpha + cost + (1 | pid),
-            data = dat, family = binomial, control = glmerControl(optimizer = "bobyqa"),
-            nAGQ = 10)
+fit_model <- function(df, strategy) {
+    glmer(as.formula(paste(strategy, "~ sigma + alpha + cost + (1 | pid)")),
+          data = df, family = binomial, control = glmerControl(optimizer = "bobyqa"), nAGQ = 10)
+}
 
-sink(file = paste(outdir,"R_TTB_SAT.txt",sep=''))
-coef(summary(s1))
-sink(file = NULL)
+write_coef <- function(strategy) {
+    coefs <- coef(summary(fit_model(dat_full, strategy)))
+    cost_coefs <- coef(summary(fit_model(dat_cost, strategy)))
+    coefs["cost",] <- cost_coefs["cost",]
 
-s2 <- glmer(SAT_TTB ~ sigma + alpha + cost + (1 | pid),
-            data = dat, family = binomial, control = glmerControl(optimizer = "bobyqa"),
-            nAGQ = 10)
+    sink(file = paste(outdir, "R_", strategy, ".txt", sep = ""))
+    print(coefs)
+    sink(file = NULL)
+}
 
-sink(file = paste(outdir,"R_SAT_TTB.txt",sep=''))
-coef(summary(s2))
-sink(file = NULL)
-
-s3 <- glmer(TTB ~ sigma + alpha + cost + (1 | pid),
-            data = dat, family = binomial, control = glmerControl(optimizer = "bobyqa"),
-            nAGQ = 10)
-
-sink(file = paste(outdir,"R_TTB.txt",sep=''))
-coef(summary(s3))
-sink(file = NULL)
-
-s4 <- glmer(WADD ~ sigma + alpha + cost + (1 | pid),
-            data = dat, family = binomial, control = glmerControl(optimizer = "bobyqa"),
-            nAGQ = 10)
-
-sink(file = paste(outdir,"R_WADD.txt",sep=''))
-coef(summary(s4))
-sink(file = NULL)
-
-s5 <- glmer(Rand ~ sigma + alpha + cost + (1 | pid),
-            data = dat, family = binomial, control = glmerControl(optimizer = "bobyqa"),
-            nAGQ = 10)
-
-sink(file = paste(outdir,"R_Rand.txt",sep=''))
-coef(summary(s5))
-sink(file = NULL)
-
-s6 <- glmer(Other ~ sigma + alpha + cost + (1 | pid),
-            data = dat, family = binomial, control = glmerControl(optimizer = "bobyqa"),
-            nAGQ = 10)
-
-sink(file = paste(outdir,"R_Other.txt",sep=''))
-coef(summary(s6))
-sink(file = NULL)
+dat_full <- prepare_data(dat)
+dat_cost <- prepare_data(dat[dat$cost > 0,])
+for (strategy in c("TTB_SAT", "SAT_TTB", "TTB", "WADD", "Rand", "Other")) {
+    write_coef(strategy)
+}
