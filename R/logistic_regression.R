fea63e1600271f2f39eefe1804ea1c103f2a0e0f
fredcallaway <fredc@llaway.com>
2026-06-29 07:04:22 -0400

    fix logistic regression rank predictors

--- R/logistic_regression.R
+++ R/logistic_regression.R
@@ -4,7 +4,18 @@ dat <- read.csv(args[2])   # path to processed human trials.csv
 
 library(lme4)
 
-s1 <- glmer(R_TTB_SAT ~ R_sigma + R_alpha + R_cost + (1 | pid),
+rank_desc <- function(x) {
+    match(x, sort(unique(x), decreasing = TRUE)) - 1
+}
+
+dat$sigma <- match(dat$sigma, sort(unique(dat$sigma))) - 1
+dat$alpha <- rank_desc(dat$alpha)
+dat$cost <- rank_desc(dat$cost)
+for (strategy in c("TTB_SAT", "SAT_TTB", "TTB", "WADD", "Rand", "Other")) {
+    dat[[strategy]] <- as.integer(dat[[strategy]] == "True")
+}
+
+s1 <- glmer(TTB_SAT ~ sigma + alpha + cost + (1 | pid),
             data = dat, family = binomial, control = glmerControl(optimizer = "bobyqa"),
             nAGQ = 10)
 
@@ -12,7 +23,7 @@ sink(file = paste(outdir,"R_TTB_SAT.txt",sep=''))
 coef(summary(s1))
 sink(file = NULL)
 
-s2 <- glmer(R_SAT_TTB ~ R_sigma + R_alpha + R_cost + (1 | pid),
+s2 <- glmer(SAT_TTB ~ sigma + alpha + cost + (1 | pid),
             data = dat, family = binomial, control = glmerControl(optimizer = "bobyqa"),
             nAGQ = 10)
 
@@ -20,7 +31,7 @@ sink(file = paste(outdir,"R_SAT_TTB.txt",sep=''))
 coef(summary(s2))
 sink(file = NULL)
 
-s3 <- glmer(R_TTB ~ R_sigma + R_alpha + R_cost + (1 | pid),
+s3 <- glmer(TTB ~ sigma + alpha + cost + (1 | pid),
             data = dat, family = binomial, control = glmerControl(optimizer = "bobyqa"),
             nAGQ = 10)
 
@@ -28,7 +39,7 @@ sink(file = paste(outdir,"R_TTB.txt",sep=''))
 coef(summary(s3))
 sink(file = NULL)
 
-s4 <- glmer(R_WADD ~ R_sigma + R_alpha + R_cost + (1 | pid),
+s4 <- glmer(WADD ~ sigma + alpha + cost + (1 | pid),
             data = dat, family = binomial, control = glmerControl(optimizer = "bobyqa"),
             nAGQ = 10)
 
@@ -36,7 +47,7 @@ sink(file = paste(outdir,"R_WADD.txt",sep=''))
 coef(summary(s4))
 sink(file = NULL)
 
-s5 <- glmer(R_Rand ~ R_sigma + R_alpha + R_cost + (1 | pid),
+s5 <- glmer(Rand ~ sigma + alpha + cost + (1 | pid),
             data = dat, family = binomial, control = glmerControl(optimizer = "bobyqa"),
             nAGQ = 10)
 
@@ -44,11 +55,10 @@ sink(file = paste(outdir,"R_Rand.txt",sep=''))
 coef(summary(s5))
 sink(file = NULL)
 
-s6 <- glmer(R_Other ~ R_sigma + R_alpha + R_cost + (1 | pid),
+s6 <- glmer(Other ~ sigma + alpha + cost + (1 | pid),
             data = dat, family = binomial, control = glmerControl(optimizer = "bobyqa"),
             nAGQ = 10)
 
 sink(file = paste(outdir,"R_Other.txt",sep=''))
 coef(summary(s6))
 sink(file = NULL)
-
